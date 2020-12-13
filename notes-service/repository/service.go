4dc5e778fed786fff36f9e262f4079810da96474
geoff <g.griffiths@resilientplc.com>
2020-12-13 18:49:50 +0000

    Bumped CDK version

--- notes-service/repository/service.go
+++ notes-service/repository/service.go
@@ -1,7 +1,7 @@
 package repository
 
 type NotesService struct {
-	Id        string `dynamo:"id,hash"`
-	Name      string `dynamo:"name"`
-	Completed bool   `dynamo:"completed"`
+	Id        string `dynamo:"id,hash" json:"id"`
+	Name      string `dynamo:"name" json:"name"`
+	Completed bool   `dynamo:"completed" json:"completed"`
 }
