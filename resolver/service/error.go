f9b719e3dd1a3aaf5abe3afd42517c5f18426716
geoff <g.griffiths@resilientplc.com>
2020-12-13 18:21:32 +0000

    Adjusted .vtl file.

--- resolver/service/error.go
+++ resolver/service/error.go
@@ -3,7 +3,7 @@ package service_resolver
 import "errors"
 
 var (
-	ErrMissingId        = errors.New("note id missing")
+	ErrMissingId        = errors.New("note id missing, oops")
 	ErrMissingName      = errors.New("service name missing")
 	ErrMissingCompleted = errors.New("completed missing")
 )
